65700dee3618bccbdc2ec64059423673e4dab243
Jérôme Lavoué <jerome.lavoue@umontreal.ca>
2021-10-29 18:14:45 +0200

    adding a graph and correcting some

--- scripts/IntrotoBayes_normal_example.R
+++ scripts/IntrotoBayes_normal_example.R
@@ -103,15 +103,15 @@ p
 
 mysample <- data.frame( x = rnorm( 1000 , mean= (tau0*mu0 + n*tau*mu)/(tau0+n*tau),sd=1/sqrt(tau0+n*tau)))
 
-p <- ggplot(data=mydata, aes(x=x))+ geom_line(aes(y=y),colour='black',size=1.0)
+p <- ggplot(data=mydata, aes(x=x))+ geom_line(aes(y=y),colour='red',size=1.0)
 
-p <- p + geom_line( aes(y=y2),colour='blue',size=1.0)
+p <- p + geom_line( aes(y=y2),colour='green',size=1.0)
 
 p <- p + geom_histogram( data= mysample ,aes(x=x,y=..density..),size=1.0, alpha = 0.5 , bins=50 )
 
 p <- p + theme_solarized()
 
-p <- p +  xlab('Taille (cm)')+
+p <- p +  xlab('Univers des hypothèse pour la taille moyenne (cm)')+
   ylab('Densité')+
   theme(axis.title.x=element_text(size=14))+
   theme(axis.title.y=element_text(size=14))+
@@ -131,3 +131,68 @@ p
 
 
 #write_xlsx( mysample , "data/myposteriorsample.xlsx" )
+
+
+#' Distribution postérieure théorique 
+
+#+ posterior theo , echo = FALSE , message = FALSE , warning = FALSE
+
+
+p <- ggplot(data=mydata, aes(x=x))+ geom_line(aes(y=y),colour='red',size=1.0)
+
+p <- p + geom_line( aes(y=y2),colour='green',size=1.0)
+
+p <- p + geom_line( aes(y=y3),colour='black',size=1.0 )
+
+p <- p + theme_solarized()
+
+p <- p +  xlab('Univers des hypothèse pour la taille moyenne (cm)')+
+  ylab('Densité')+
+  theme(axis.title.x=element_text(size=14))+
+  theme(axis.title.y=element_text(size=14))+
+  theme(axis.text.x=element_text(size=12 , hjust = 0.5))+
+  theme(axis.text.y=element_text(size=12 ,  hjust = 0.5))
+
+p <- p + scale_y_continuous(expand = c(0 , 0.01) , 
+                            limits = c(0,0.30),
+                            labels = scales::number_format(accuracy = 0.01,
+                                                           decimal.mark = ',') )
+
+p <- p + scale_x_continuous(limits = c(150,200),
+                            labels = scales::number_format(accuracy = 0.01,
+                                                           decimal.mark = ',') )
+
+p
+
+
+
+#' Distribution postérieure simulée par échantillonnage (n=1000) (uniquement la postérieure)
+
+#+ posterior simulation2 , echo = FALSE , message = FALSE , warning = FALSE
+
+
+p <- ggplot(data=mysample)
+
+p <- p + geom_histogram( aes(x=x),size=1.0, alpha = 0.5 , bins=50 )
+
+p <- p + theme_solarized()
+
+p <- p +  xlab('Univers des hypothèse pour la taille moyenne (cm)')+
+  ylab('fréquence')+
+  theme(axis.title.x=element_text(size=14))+
+  theme(axis.title.y=element_text(size=14))+
+  theme(axis.text.x=element_text(size=12 , hjust = 0.5))+
+  theme(axis.text.y=element_text(size=12 ,  hjust = 0.5))
+
+p <- p + scale_y_continuous(expand = c(0 , 0.01) , 
+                            #limits = c(0,0.30),
+                            labels = scales::number_format(accuracy = 0.01,
+                                                           decimal.mark = ',') )
+
+p <- p + scale_x_continuous(limits = c(180,200),
+                            labels = scales::number_format(accuracy = 0.01,
+                                                           decimal.mark = ',') )
+
+p
+
+
