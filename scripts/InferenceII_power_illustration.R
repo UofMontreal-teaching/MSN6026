9cb1719c54b0b116ca86e7e66308fd6af815fad0
Jérôme Lavoué <jerome.lavoue@umontreal.ca>
2023-09-25 15:13:50 -0400

    WIP power

--- scripts/InferenceII_power_illustration.R
+++ scripts/InferenceII_power_illustration.R
@@ -18,6 +18,7 @@ library(ggplot2)
 library(ggthemes)
 library(writexl)
 library(scales)
+library(dplyr)
 
 
 #+ initial calculations , include = FALSE , echo = FALSE
@@ -39,29 +40,32 @@ n.sim <-10000
 
 #+ rejection region , include = FALSE , echo = FALSE
 
-mydata <-data.frame(x=seq(-3.5,3.5,length=1000))
+mydata <-data.frame(x=seq(-4,15,length=1000))
 mydata$y<- dt(mydata$x,(n-1))
 
-ggplot(data=mydata, aes(x=x))+  
-  geom_line(aes(y=y),colour='black',size=1.5)+	
-  geom_ribbon(data=subset(mydata,x<=qt( (1-alpha) ,(n-1) )),aes(ymax=y),ymin=0,
-              fill="springgreen3",colour=NA,alpha=I(1/3))+
-  geom_ribbon(data=subset(mydata,x>qt( (1-alpha) ,(n-1) )),aes(ymax=y),ymin=0,
-              fill="red",colour=NA,alpha=I(1/3))+
-  geom_segment(aes(x = qt( (1-alpha) ,(n-1) ), y = 0, xend = qt( (1-alpha) ,(n-1) ), yend = dt(qt( (1-alpha) ,(n-1) ),(n-1))),size=1)+                                                                                        
-  #ylim(0,0.4)+
-  xlab('Valeur St')+
-  ylab('Densité')+
-  theme(legend.position = "none") +
-  #theme(aspect.ratio = 2/(1+sqrt(5)))+
-  theme(axis.title.x=element_text(vjust=-0.5,size=20))+
-  theme(axis.title.y=element_text(size=20,angle=90))+
-  theme(axis.text.x=element_text(size=20))+
-  theme(axis.text.y=element_text(size=10,angle=90)) +
-  theme_solarized()
+p <- ggplot(data=mydata, aes(x=x)) 
+
+p <- p +   geom_line(aes(y=y),colour='black',size=1.5)	
+p <- p +   geom_ribbon(data=subset(mydata,x<=qt( (1-alpha) ,(n-1) )),aes(ymax=y),ymin=0,
+              fill="steelblue1",colour=NA,alpha=I(1/3))
+p <- p +   geom_ribbon(data=subset(mydata,x>qt( (1-alpha) ,(n-1) )),aes(ymax=y),ymin=0,
+              fill="red",colour=NA,alpha=I(1/3))
+p <- p +   geom_segment(aes(x = qt( (1-alpha) ,(n-1) ), y = 0, xend = qt( (1-alpha) ,(n-1) ), yend = dt(qt( (1-alpha) ,(n-1) ),(n-1))),size=1)                                                                                        
+
+p <- p + xlab('') +  ylab('Densité')
+
+p <- p +  theme(axis.text.x=element_text(size=18),axis.title.x=element_text(size=18,vjust=+0.55))+
+          theme(axis.text.y=element_text(size=14),axis.title.y=element_text(size=18,vjust=+0.55)) 
+
+p <- p +  scale_x_continuous( limits=c(-4,15) )
+p <- p +  scale_y_continuous( limits=c(0,0.4) )
+
+p  
 
 
-#+ power main example , include = FALSE , echo = FALSE
+
+#+ calculations , include = FALSE , echo = FALSE
+
 
 mysample1.1 <- rnorm( n.sim*n , mean= muH1.1 , sd=sigmaH1.1 )
 
@@ -75,25 +79,55 @@ mysample1.2 <- matrix(mysample1.2, nrow = n.sim , ncol = n)
 
 my.st1.1 <- apply( mysample1.1 , 1 , function(x) { (mean(x) - muH0)/(sigmaH1.1/sqrt(n))})
 my.st1.2 <- apply( mysample1.2 , 1 , function(x) { (mean(x) - muH0)/(sigmaH1.2/sqrt(n))})
-my.st1.3 <- apply( mysample , 1 , function(x) { (mean(x) - muH0)/(sigmaH1.1/sqrt(n))})
-my.st1.4 <- apply( mysample , 1 , function(x) { (mean(x) - muH0)/(sigmaH1.1/sqrt(n))})
+#my.st1.3 <- apply( mysample , 1 , function(x) { (mean(x) - muH0)/(sigmaH1.1/sqrt(n))})
+#my.st1.4 <- apply( mysample , 1 , function(x) { (mean(x) - muH0)/(sigmaH1.1/sqrt(n))})
 
 
 my.emp.st <- data.frame(x1.1=my.st1.1)
 my.emp.st$x1.2 <-my.st1.2
 
-mydata <-data.frame(x=seq(-3.5,10,length=5000))
-mydata$st.theo<- dt( mydata$x - (muH1.1-muH0)/(sigmaH1.1/sqrt(n))  , (n-1) )
+my.emp.st$breaks1.1 <- cut(my.emp.st$x1.1, breaks = qt( (1-alpha) ,(n-1) ) )
 
-#theorethical st distribution
-p <- ggplot(data=mydata, aes(x=x))+ geom_line(aes(y=st.theo),colour='black',size=1.0)
+#+ power 1 diff variab, include = FALSE , echo = FALSE
+
+
+# color change for x
+
+my.emp.st <-
+  mutate(my.emp.st,
+    grp = cut(x1.1, seq(-4, 15, by = 0.1), labels = FALSE, include.lowest = TRUE),
+    is6 = between(grp, 60, 90)
+  ) 
+
+  ggplot(aes(x, fill = is6)) +
+  geom_histogram(binwidth = 0.01, boundary = 0, closed = "left", 
+                 col = "darkgreen", alpha = 0.5, size = 0.1) +
+  scale_x_continuous(breaks = seq(0, 1, 0.1))
+
+# empirical distributionb of st
+
+p <- ggplot(data=my.emp.st )
+
+#empirical st
+p <- p + geom_histogram( data= my.emp.st ,aes(x=x1.1,  col = is6, fill = is6), binwidth=0.01 ,size=1.0 )
 
 #critical value
 p <- p +  geom_segment(aes(x = qt( (1-alpha) ,(n-1) ), y = 0, xend = qt( (1-alpha) ,(n-1) ), yend = dt( qt(  1-alpha , (n-1) )- (muH1.1-muH0)/(sigmaH1.1/sqrt(n)) ,(n-1))),size=1)                                                        
-#empirical st
-p <- p + geom_density( data= my.emp.st ,aes(x=x1.1,y=..density..),size=1.0,  color = "green" )
 
-p <- p + geom_density( data= my.emp.st ,aes(x=x1.2,y=..density..),size=1.0,  color = "darkgreen" )
+#beauty
+
+p <- p + xlab('') +  ylab('Densité')
+
+p <- p +  theme(axis.text.x=element_text(size=18),axis.title.x=element_text(size=18,vjust=+0.55))+
+  theme(axis.text.y=element_text(size=14),axis.title.y=element_text(size=18,vjust=+0.55)) 
+
+p <- p +  scale_x_continuous( limits=c(-4,15) )
+p <- p +  scale_y_continuous( limits=c(0,0.8) )
+
+p 
+
+
+
 
 ### TO BE CONTINUED
 
